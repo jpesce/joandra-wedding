3a4060f8ba382370211db5c08f3a140076fb2050
João Pesce <jpesce@gmail.com>
2022-12-27 16:59:47 -0300

    feat: add calendar and maps links in event details

--- src/components/EventDetails/index.tsx
+++ src/components/EventDetails/index.tsx
@@ -5,7 +5,6 @@ interface EventInformationProps {
   name: string;
   children: JSX.Element | string;
 }
-
 const EventInformation = ({
   name,
   children,
@@ -34,12 +33,30 @@ const EventDetails = (): JSX.Element => {
       <div className="basis-1/2 px-16 py-20 text-joanGreen-500">
         <dl className="flex flex-col space-y-8">
           <EventInformation name="Que dia?">
-            Sábado, cinco de agosto de dois mil e vinte e três
+            <>
+              Sábado, cinco de agosto de dois mil e vinte e três
+              <a
+                className="nowrap relative top-[-2px] ml-4 select-none whitespace-nowrap rounded-full border border-joanGreen-500 py-[0.25rem] px-[0.75rem] align-middle font-sans text-sm hover:bg-joanGreen-500 hover:text-white"
+                href="https://calendar.google.com/calendar/u/0/r/eventedit?dates=20230805T173000Z/20230806T010000Z&text=Casamento+Chandra+%26+Jo%C3%A3o&location=Av.+Professor+Cl%C3%B3vis+Salgado,+1485+-+Bandeirantes,+Belo+Horizonte/MG&details=https://chandraejoao.io/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Google Calendar ＋
+              </a>
+            </>
           </EventInformation>
           <EventInformation name="Que horas?">Três da tarde</EventInformation>
           <EventInformation name="Onde?">
             <address className="not-italic">
               Av. Professor Clóvis Salgado, 1485 — Bandeirantes, BH/MG
+              <a
+                className="nowrap relative top-[-2px] ml-4 select-none whitespace-nowrap rounded-full border border-joanGreen-500 py-[0.25rem] px-[0.75rem] align-middle font-sans text-sm hover:bg-joanGreen-500 hover:text-white"
+                href="https://goo.gl/maps/i56RHKW5uLg1YMg78"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Google Maps ↗
+              </a>
             </address>
           </EventInformation>
         </dl>
