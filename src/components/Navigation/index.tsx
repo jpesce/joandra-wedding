bda1db141858187e6a07779e845ef30ec7b34ae4
João Pesce <jpesce@gmail.com>
2023-01-02 15:10:14 -0300

    feat: add mobile navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -2,28 +2,33 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import logo from "../../../public/chandra-e-joao.svg";
 
-const Navigation = (): JSX.Element => {
-  const [textClass, setTextClass] = useState(
-    "text-white selection:bg-white selection:text-joanGreen-600"
-  );
-  const [animation, setAnimation] = useState("animate-wiggle");
+type Background = "dark" | "light";
+type SetBackground = React.Dispatch<React.SetStateAction<Background>>;
 
-  const scrollListener = () => {
-    if (window.scrollY < window.innerHeight - 63) {
-      setTextClass(
-        "text-white selection:bg-white selection:text-joanGreen-600"
-      );
-    } else {
-      setTextClass("text-black selection:bg-black selection:text-white");
-    }
-  };
+const navigationScrollListener = (setBackground: SetBackground) => {
+  window.scrollY < window.innerHeight - 63
+    ? setBackground("dark")
+    : setBackground("light");
+};
+
+const Navigation = (): JSX.Element => {
+  const [background, setBackground] = useState<Background>("dark");
+  const [animate, setAnimate] = useState(true);
 
   useEffect(() => {
-    window.addEventListener("scroll", scrollListener);
-    window.addEventListener("resize", scrollListener);
+    window.addEventListener("scroll", () =>
+      navigationScrollListener(setBackground)
+    );
+    window.addEventListener("resize", () =>
+      navigationScrollListener(setBackground)
+    );
     return () => {
-      window.removeEventListener("scroll", scrollListener);
-      window.removeEventListener("resize", scrollListener);
+      window.removeEventListener("scroll", () =>
+        navigationScrollListener(setBackground)
+      );
+      window.removeEventListener("resize", () =>
+        navigationScrollListener(setBackground)
+      );
     };
   }, []);
 
@@ -31,18 +36,30 @@ const Navigation = (): JSX.Element => {
     <>
       <a
         href="#hero"
-        onMouseEnter={() => setAnimation("animate-wiggle")}
-        onAnimationEnd={() => setAnimation("")}
-        className={`fixed top-8 left-12 z-40 w-[7rem] ${animation}`}
+        onMouseEnter={() => setAnimate(true)}
+        onAnimationEnd={() => setAnimate(false)}
+        className={`fixed top-4 z-40 w-full lg:top-8 lg:left-12 lg:w-auto ${
+          animate ? "animate-wiggle" : ""
+        }`}
       >
         <Image
           src={logo}
           alt="Chandra e João"
-          className="select-none"
+          className="m-auto select-none"
           priority={true}
         />
       </a>
-      <nav className={`fixed top-14 right-20 space-x-8 ${textClass} z-40`}>
+      <nav
+        className={`fixed top-14 right-20 hidden space-x-8 lg:block ${
+          background === "dark"
+            ? "text-white selection:bg-white selection:text-joanGreen-600"
+            : ""
+        }${
+          background === "light"
+            ? "text-black selection:bg-black selection:text-white"
+            : ""
+        } z-40`}
+      >
         <a href="#a-festa">A festa</a>
         <a href="#lista-de-presentes">Lista de presentes</a>
         <a href="#confirmar-presenca">Confirmar presença</a>
