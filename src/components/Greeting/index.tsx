b5c603e043340076988a69a88304ed5c218bcfbe
Chandra Drummond <121632487+chandradrummond@users.noreply.github.com>
2023-01-20 17:36:40 -0300

    edit: change food & drinks and greeting text

--- src/components/Greeting/index.tsx
+++ src/components/Greeting/index.tsx
@@ -5,11 +5,15 @@ const Intro = (): JSX.Element => {
     <div className="flex justify-center border-t border-joanGreen-600 py-10 px-8 text-center selection:bg-joanGreen-600 selection:text-white lg:py-20">
       <div className="max-w-[36rem] space-y-6 uppercase text-joanGreen-600">
         <p>
-          Vista uma roupa para se sentir especial e venha confortável para um
-          dia de festa com música, bebidas, drinks, antepastos e comidinhas.
-          Gostoso demais.Vista uma roupa para se sentir especial e venha
-          confortável para um dia de festa com música, bebidas, drinks,
-          antepastos e comidinhas. Gostoso demais.
+          Depois de anos comemorando todos os dias entre nós, decidimos levar 
+          essa festa para as pessoas mais especiais ao nosso redor. Pensamos e
+          preparamos um momento pra traduzir a gente nesse dia, e é com muita 
+          alegria mesmo que a gente te chama pra fazer parte dele. Como 
+          qualquer festa nossa, espere um mesa de antepastos recheada, no 
+          melhor esquema cada um por si, com um pouco de tudo. Bebidas, drinks
+          especiais, muita música e um jantar pra fechar com chave de ouro. Se
+          vista pra esse dia único e venha pronto pra aproveitar muito do nosso
+          lado!
         </p>
         <Signatures className="m-auto h-12 max-w-full text-black lg:h-14" />
       </div>
