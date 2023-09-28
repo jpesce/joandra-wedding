53db72fa54b1c5413d50a6ee94217b599dbb892f
João Pesce <jpesce@gmail.com>
2023-09-28 16:46:43 -0300

    edit: use top-level smooth scroll polyfill
    Move smooth scroll polyfill to a top-level import in `_app.tsx` as per
    Next.js documentation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,12 @@
-import { useEffect } from "react";
+import "smoothscroll-polyfill";
 
 import type { AppProps } from "next/app";
 import type { AppType } from "next/dist/shared/lib/utils";
-
-import { polyfill as smoothScrollPolyfill } from "smoothscroll-polyfill";
 
 import "../styles/globals.css";
 import { serif, condensed } from "../fonts/localFonts";
 
 const App: AppType = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    smoothScrollPolyfill();
-  }, []);
-
   return (
     <div className={`${serif.variable} ${condensed.variable}`}>
       <Component {...pageProps} />
